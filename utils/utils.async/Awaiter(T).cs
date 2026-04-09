881b8c93293d477e26fae7ff2b91df2b23fff3ec
Pr-Barnart <59121080+Pr-Barnart@users.noreply.github.com>
2026-04-09 10:54:04 +0200

    Add files via upload

---
+++ utils/utils.async/Awaiter(T).cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace utils
+{
+    public class Awaiter<T>
+    {
+        private readonly TaskCompletionSource<T> _tcs =
+            new TaskCompletionSource<T>();
+
+        public Task<T> Task => _tcs.Task;
+
+        public Awaiter()
+        {
+        }
+
+        public Awaiter(CancellationToken ct, object owner = null)
+        {
+            if (ct.IsCancellationRequested)
+            {
+                Release(owner);
+            }
+            else
+            {
+                ct.Register(() => Release(owner));
+            }
+        }
+
+        private void Release(object owner)
+        {
+            if (owner is IDisposable disposable)
+            {
+                disposable.Dispose();
+            }
+
+            Cancel();
+        }
+
+        public bool CompleteWithSuccess(T result)
+        {
+            return _tcs.TrySetResult(result);
+        }
+
+        public bool CompleteWithError(Exception error)
+        {
+            return _tcs.TrySetException(error);
+        }
+
+        public bool Cancel()
+        {
+            return _tcs.TrySetCanceled();
+        }
+        public TaskAwaiter<T> GetAwaiter()
+        {
+            return _tcs.Task.GetAwaiter();
+        }
+    }
+}
---
+++ utils/utils.async/Awaiter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace utils
+{
+    public class Awaiter
+    {
+        private readonly TaskCompletionSource<bool> _tcs =
+    new TaskCompletionSource<bool>();
+
+        public Task Task => _tcs.Task;
+
+        public Awaiter()
+        {
+        }
+
+        public Awaiter(CancellationToken ct, object owner = null)
+        {
+            if (ct.IsCancellationRequested)
+            {
+                Release(owner);
+            }
+            else
+            {
+                ct.Register(() => Release(owner));
+            }
+        }
+
+        private void Release(object owner)
+        {
+            if (owner is IDisposable disposable)
+            {
+                disposable.Dispose();
+            }
+
+            Cancel();
+        }
+
+        public bool CompleteWithSuccess()
+        {
+            return _tcs.TrySetResult(true);
+        }
+
+        public bool CompleteWithError(Exception error)
+        {
+            return _tcs.TrySetException(error);
+        }
+
+        public bool Cancel()
+        {
+            return _tcs.TrySetCanceled();
+        }
+        public TaskAwaiter <bool> GetAwaiter()
+        {
+            return _tcs.Task.GetAwaiter();
+        }
+    }
+}
